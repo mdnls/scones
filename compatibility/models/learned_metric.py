b4234d2bba8c1cee2515c32170dcade0d0086ed2
Max Daniels <daniels.g@northeastern.edu>
2021-05-14 13:25:09 -0400

    Implement LearnedMetric and FacenetMetric

---
+++ compatibility/models/learned_metric.py
@@ -0,0 +1,51 @@
+from compatibility.models import get_cost
+from facenet_pytorch import InceptionResnetV1
+import torch.nn as nn
+from datasets import inverse_data_transform
+
+
+class LearnedMetric(nn.Module):
+    def __init__(self, repr_source, repr_target, repr_cost):
+        '''
+        Implements a learned metric to compare data, of the form d(x, y) = c(Rs(x), Rt(y)) where Rs, Rt map x, y respectively
+            to some representation vector and where c is a l2, l2-squared, or other cost.
+        Args:
+            repr_source: the function Rs which maps source distribution data to representation vectors
+            repr_target: the function Rs which maps source distribution data to representation vectors
+            repr_cost: choice of c. Either 'l2-sq' or 'mean-l2-sq'.
+        '''
+        super(LearnedMetric, self).__init__()
+
+        assert repr_cost in ['l2-sq', 'mean-l2-sq'], f"{repr_cost} is an invalid choice of repr_cost."
+        self.repr_source = repr_source
+        self.repr_target = repr_target
+        self.repr_cost = get_cost(repr_cost)
+
+    def forward(self, x, y):
+        return self.repr_cost(self.repr_source(x), self.repr_target(y))
+
+
+class FacenetMetric(nn.Module):
+    def __init__(self, config):
+        '''
+        A learned metric between distances which computes a Euclidean distance between a learned representation for each
+            image.
+
+        Args:
+            repr_cost: Euclidean cost to use between learned image representations. One of ['l2-sq', 'mean-l2-sq'].
+        '''
+        super(FacenetMetric, self).__init__()
+        self.resnet = InceptionResnetV1(pretrained="vggface2").eval()
+        self.resize = nn.Upsample(size=160, mode="bilinear", align_corners=True) # 160px is default image size
+        self.metric = LearnedMetric(repr_source=self.repr, repr_target=self.repr, repr_cost='mean-l2-sq')
+        self.config = config
+
+    def preprocess(self, img_batch):
+        resized_batch = self.resize(img_batch)
+        return 2 * inverse_data_transform(self.config, resized_batch) - 1
+
+    def repr(self, img_batch):
+        return self.resnet(self.preprocess(img_batch))
+
+    def forward(self, x, y):
+        return self.metric(x, y)
